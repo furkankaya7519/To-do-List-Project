0b07a3f0d51ccf202c56e292dfb0e5f9bdd9b735
Furkan Kaya <furkankaya7519@gmail.com>
2023-02-14 22:38:51 +0300

    feat: Added RemoveAllButton Script

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 
 const addButton = document.querySelector("#addButton");
+const removeAllButton = document.querySelector("#removeAllButton");
 const todoInput = document.querySelector("#todoInput");
 const todoListGroup = document.querySelector("#todoListGroup");
 let todoArray = [];
+let todoList;
+
 
 
 showTodos();
@@ -13,6 +16,10 @@ addListeners();
 
 function showTodos(){
     
+    if(localStorage.getItem("todoList") == null){
+        return;
+    }
+
     if(localStorage.getItem("todoList") != null){
         let str = localStorage.getItem("todoList");
         todoArray = str.split(",");
@@ -22,14 +29,37 @@ function showTodos(){
         }
     }
     
-    
+    todoList = document.querySelectorAll(".tD");
 }
 
 
 
 function addListeners(){
     addButton.addEventListener("click",addButtonListener);
+    removeAllButton.addEventListener("click",removeAllButtonListener);
 }
+
+
+
+
+
+
+function removeAllButtonListener(){
+    
+    localStorage.removeItem("todoList");
+
+    let myList = document.querySelectorAll(".tD");
+    
+    for (const m of myList) {
+        m.remove();
+    }
+
+    
+}
+
+
+
+
 
 
 
@@ -39,6 +69,7 @@ function addButtonListener(){
         return;
     }
     addToDoElement(todoInput.value,1);
+    todoInput.value = "";
 
 }
 
@@ -51,8 +82,9 @@ function checkTodoInput(){
 }
 
 function addToDoElement(t,w){
+
     const listElement = document.createElement("li");
-    listElement.className = "list-group-item list-group-item-action";
+    listElement.className = "list-group-item list-group-item-action tD";
     listElement.innerHTML = t;
     todoListGroup.appendChild(listElement);
     
@@ -62,7 +94,7 @@ function addToDoElement(t,w){
     
 
     let str = localStorage.getItem("todoList");
-    if(str === null){
+    if(str == null){
         localStorage.setItem("todoList", todoArray.toString());
     }
     else{
